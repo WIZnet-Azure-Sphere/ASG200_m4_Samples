ec80174802ae9d572224da5bcd389e1cc3b97d47
joon874 <lawrence@wiznet.io>
2020-06-08 16:24:49 +0900

    init release

---
+++ Sample_Code/BareMetal/DHCP_Server/out/ARM-Debug/lib/MT3620_M4_Driver/cmake_install.cmake
@@ -0,0 +1,40 @@
+# Install script for directory: E:/__TeamB/__git/wiznet-azure-sphere-git/ASG200_m4_Samples/MT3620_M4_Driver
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "E:/__TeamB/__git/wiznet-azure-sphere-git/ASG200_m4_Samples/Sample_Code/BareMetal/DHCP_Server/install/ARM-Debug")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("E:/__TeamB/__git/wiznet-azure-sphere-git/ASG200_m4_Samples/Sample_Code/BareMetal/DHCP_Server/out/ARM-Debug/lib/MT3620_M4_Driver/lib/MT3620_M4_BSP/cmake_install.cmake")
+
+endif()
+
